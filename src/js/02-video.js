6573a7da6704b8b2c3b6ff997b97dfb5fd9a45fe
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-03-12 19:06:13 +0200

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import trottle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// const currentTime = localStorage.getItem('videoplayer-current-time')
+
 
 player.on('timeupdate', trottle(getCurrentTime, 1000)); 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
 import trottle from 'lodash.throttle';
+const formInputEl = document.querySelector(".feedback-form");
+const emailEl = formInputEl.email;
+const messageEl = formInputEl.message;
+
+ formInputEl.addEventListener("input", trottle(getCurrentValue, 500))
+
+function getCurrentValue(event) {
+   
+    const {
+      elements: { email, message },
+      } = event.currentTarget;
+      const form = {email: email.value, message: message.value};
+    console.log(form);
+   
+localStorage.setItem('feedback-form-state', JSON.stringify({email: email.value, message: message.value}));
+
+}
+
+
+formInputEl.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === "" || message.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const user = {email: email.value, message: message.value};
+  console.log(user);
+ localStorage.removeItem('feedback-form-state')
+  event.currentTarget.reset();
+ }
+
+
+ const fillForm = () => {
+    const objFromLocalStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  
+    if (objFromLocalStorage) {
+        emailEl.value = objFromLocalStorage.email || '';
+        messageEl.value=objFromLocalStorage.message || '';
+
+    }
+ }
+ fillForm()
+
+
+
