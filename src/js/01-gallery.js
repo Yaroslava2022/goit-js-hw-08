b23704087634c0caa3751da5c7b242ae2d79b69f
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-03-12 11:59:33 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 const makeGalleryCard = ({
