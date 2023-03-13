5ebb34308100f169b622a13f372977f867956aef
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-03-13 20:50:38 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,8 +4,6 @@ import trottle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-
-
 player.on('timeupdate', trottle(getCurrentTime, 1000)); 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
